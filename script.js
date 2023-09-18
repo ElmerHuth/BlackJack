ad71f446e6b7826afb41779a2c5bec237ebf2032
Elmer <elmer.huth@edu.huddinge.se>
2023-09-18 13:05:11 +0200

    fixed the shuffle function and added function to convert cards number notation e.g. [13, 2] to a readable string => King of diamonds

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // ace = 1, knight = 11, queen = 12, king = 13
-// hearts = 1, diamonds = 2, spades = 3, clover = 4
+// hearts = 1, diamonds = 2, spades = 3, clovers = 4
 // deck = [..., [13, 2], ...] 
 // first number is value and second is sleeve in this case: king of diamonds
 
-function shuffle(inputDeck){
-    let outputDeck = []
+function shuffle(Deck){
+    let inputDeck = Deck;
+    let outputDeck = [];
     while (outputDeck.length < 52){
-        outputDeck.push(inputDeck[Math.floor(Math.random() * (outputDeck.length + 1))]);
+        let randomindex = Math.floor(Math.random() * (inputDeck.length - 1 + 1))
+        outputDeck.push(inputDeck[randomindex]);
+        inputDeck.splice(randomindex, 1);
     }
     return outputDeck;
 }
@@ -23,5 +26,70 @@ function createDeck(){
     return outputDeck;
 }
 
+function printCard([value, sleeve]){
+    let card = '';
+    switch (value) {
+        case 1:
+            card += 'Ace'
+            break;
+        case 2:
+            card += 'Two'
+            break;
+        case 3:
+            card += 'Three'
+            break;
+        case 4:
+            card += 'Four'
+            break;
+        case 5:
+            card += 'Five'
+            break;
+        case 6:
+            card += 'Six'
+            break;
+        case 7:
+            card += 'Seven'
+            break;
+        case 8:
+            card += 'Eight'
+            break;
+        case 9:
+            card += 'Nine'
+            break;
+        case 10:
+            card += 'Ten'
+            break;
+        case 11:
+            card += 'Knight'
+            break;
+        case 12:
+            card += 'Queen'
+            break;
+        case 13:
+            card += 'King'
+            break;
+    }
+    card += ' of '
+    switch (sleeve){
+        case 1:
+            card += "hearts"
+            break;
+        case 2:
+            card += "diamonds"
+            break;
+        case 3:
+            card += "spades"
+            break;
+        case 4:
+            card += "clovers"
+            break;
+    }
+    return card
+}
+
 let deck = createDeck();
 deck = shuffle(deck);
+for (let i = 0; i < deck.length; i++){
+    console.log(printCard(deck[i]));
+}
+console.log(deck.length);
