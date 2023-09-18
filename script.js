8e983fa1c23f5ca3748feb8f79e3288a5aec3cbb
Elmer <elmer.huth@edu.huddinge.se>
2023-09-18 12:34:48 +0200

    added function to shuffle deck

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let deck = [];
-
 // ace = 1, knight = 11, queen = 12, king = 13
 // hearts = 1, diamonds = 2, spades = 3, clover = 4
 // deck = [..., [13, 2], ...] 
 // first number is value and second is sleeve in this case: king of diamonds
 
+function shuffle(inputDeck){
+    let outputDeck = []
+    while (outputDeck.length < 52){
+        outputDeck.push(inputDeck[Math.floor(Math.random() * (outputDeck.length + 1))]);
+    }
+    return outputDeck;
+}
+
+// first loop is for the 4 sleeves, second loop is for the value ace -> king
 for (let i = 0; i < 4; i++){
     for (let x = 0; x < 13; x++){
+        // creates the card in the correct position in the deck
         deck[13 * i + x] = [x + 1, i + 1];
     }
 }
 
-for(let i = 0; i < deck.length; i++){
-    console.log(deck[i]);
-}
+let deck = [];
+deck = shuffle(deck);
